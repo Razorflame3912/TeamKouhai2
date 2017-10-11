f3f2164905166b312ac9463985144ef5a8703757
Jake Zaia <jzaia@stuy.edu>
2017-10-11 17:35:29 -0400

    Finished touchups

--- login.py
+++ login.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, session, redirect, url_for, f
 app = Flask(__name__)
 app.secret_key = 'THE TING GO SKRRRA PA PA KA KA KA'
 
-
 @app.route('/', methods=['GET','POST'])
 def root():
     if 'logout' in request.form:
@@ -18,26 +17,27 @@ def root():
 def welcome():
     if 'user' not in session:
         if 'username' not in request.form or request.form['username'] == u'':
-            flash('Bad username.')
+            flash('Error: Bad username.')
             return redirect(url_for('root'))
         else:
             enteredUser = request.form['username']
             # print repr(enteredUser)
 
         if 'pass' not in request.form or request.form['pass'] == u'':
-            flash('Bad password. Make it better!')
+            flash('Error: Bad password.')
             return redirect(url_for('root'))
 
         else:
             enteredPass = request.form['pass']
             # print enteredPass
+            
         if enteredUser == 'Luke' and enteredPass == 'fourwordsalluppercase':
             session['user'] = enteredUser
             session['pass'] = enteredPass
         else:
-            flash('Could not authenticate.')
+            flash('Error: Could not authenticate.')
             return redirect(url_for('root'))
-    return render_template('welcome.html',name=session['user'])
+    return render_template('welcome.html', name=session['user'])
 
 if __name__ == '__main__':
     app.debug = True
